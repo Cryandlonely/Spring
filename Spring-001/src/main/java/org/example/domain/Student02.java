57aa2e9b9ecba83a2f0a3d04b49b1723d9d3cef3
李兴源 <3416351474@qq.com>
2022-05-05 20:37:23 +0800

    5

---
+++ Spring-001/src/main/java/org/example/domain/Student02.java
@@ -0,0 +1,22 @@
+package org.example.domain;
+
+public class Student02 {
+    private String name;
+    private String id;
+    private School school;
+
+    @Override
+    public String toString() {
+        return "student02{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", school=" + school +
+                '}';
+    }
+
+    public Student02(String name, String id, School school) {
+        this.name = name;
+        this.id = id;
+        this.school = school;
+    }
+}
--- Spring-001/src/test/java/org/example/AppTest.java
+++ Spring-001/src/test/java/org/example/AppTest.java
@@ -5,22 +5,44 @@ import static org.junit.Assert.assertTrue;
 import org.example.domain.School;
 import org.example.domain.Student;
 import org.example.domain.Student01;
+import org.example.domain.Student02;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppTest 
 {
+    /*
+        简单类型注入
+    */
     @Test
     public void TestApp(){
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
         Student stu = (Student) ac.getBean("student");
         System.out.println(stu);
     }
+    /*
+        引用类型注入
+    */
     @Test
     public void TestApp01(){
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
         Student01 student01 = (Student01) ac.getBean("student01");
         System.out.println(student01);
     }
+    /*
+        构造方法参数名称注入
+     */
+    @Test
+    public void TestApp02(){
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student02 student02 = (Student02) ac.getBean("student02");
+        System.out.println(student02);
+    }
+    /*
+        构造方法参数下标注入
+     */
+    /*
+        构造方法参数默认顺序注入
+     */
 }
